38779c00c6e9fb9c6e91cfcebb735ef2388f2e12
Rhishikesh Ravindra Chaughule <rhishikeshchaughule@gmail.com>
2022-06-27 20:07:43 +0530

    commit

---
+++ Searching_Sorting/mergePractice.java
@@ -0,0 +1,15 @@
+public class mergePractice {
+    public static void main(String[] args) {
+        int arr[] = {19,28,3,47,57,1};
+
+        mergeSort(arr);
+        System.out.println("Array is: ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+    }
+
+    static void mergeSort(int[] arr){
+        
+    }
+}
